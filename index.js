6088abe6474445679efa1339bef975a61a9f6aa8
mohdaamir07 <83464071+mohdaamir07@users.noreply.github.com>
2022-01-20 21:46:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var d=document.querySelectorAll(".drum");
+for(var i=0;i<d.length;i++){
+  d[i].addEventListener("click",function (){
+    var k=this.innerHTML;
+    makeSound(k);
+  });
+}
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+});
+function makeSound(key){
+  switch (key) {
+    case "w":
+    var w=new Audio("sounds/tom-1.mp3");
+    w.play();
+      break;
+    case "a":
+    var a=new Audio("sounds/tom-2.mp3");
+    a.play();
+      break;
+    case "s":
+    var s=new Audio("sounds/tom-3.mp3");
+    s.play();
+      break;
+    case "d":
+    var d=new Audio("sounds/tom-4.mp3");
+    d.play();
+      break;
+    case "j":
+    var j=new Audio("sounds/snare.mp3");
+    j.play();
+      break;
+    case "k":
+    var k=new Audio("sounds/crash.mp3");
+    k.play();
+      break;
+    case "l":
+    var l=new Audio("sounds/kick-bass.mp3");
+    l.play();
+      break;
+    default:console.log(key);
+}
+}
